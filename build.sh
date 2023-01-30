7a338a0ac13c39afa65745b69f03fe046a156044
Bartosz Rybarski <bartosz.rybarski@swmansion.com>
2023-01-30 18:35:47 +0100

    Added min ios and macos versions

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -e
 ios_version=$(xcrun --sdk iphoneos --show-sdk-platform-version)
 macosx_version=$(xcrun --sdk macosx --show-sdk-platform-version)
 
+ios_min_version="13.0"
+macosx_min_version="12.0"
+
 # When changing targets or sdk_names array, make sure you update
 # indexes in fat binary creation at the end of the file.
 
@@ -40,11 +43,14 @@ for (( i=0; i < $targets_size; i++ )); do
 
   if [[ sdk_names[i] == macosx* ]]; then
     system_name="Darwin";
+    min_version="$macosx_min_version";
   else 
     system_name="iOS";
+    min_version="$ios_min_version";
   fi
 
-  flags="--sysroot $sdk_sysroot -target ${targets[i]}"
+  # Get the architecture prefix from the target triple
+  arch=$(echo ${targets[i]} | cut -d'-' -f 1)
 
   cmake \
     -DCMAKE_BUILD_TYPE=Release \
@@ -54,6 +60,8 @@ for (( i=0; i < $targets_size; i++ )); do
     -DCMAKE_CXX_COMPILER_WORKS="1" \
     -DCMAKE_SYSTEM_NAME="$system_name" \
     -DCMAKE_OSX_SYSROOT="$sdk_sysroot" \
+    -DCMAKE_OSX_ARCHITECTURES="$arch" \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET="$min_version" \
     ../..
 
   popd
@@ -90,6 +98,8 @@ lipo -create \
   Binaries/${targets[4]}/libcrypto_c_exports.dylib \
   -output Frameworks/${sdk_names[3]}/libcrypto_c_exports.framework/libcrypto_c_exports
 
+plist_cmd="/usr/libexec/PlistBuddy"
+
 for binary in $(printf "%s\n" "${sdk_names[@]}" | sort -u); do
   install_name_tool -id @rpath/libcrypto_c_exports.framework/libcrypto_c_exports ./Frameworks/$binary/libcrypto_c_exports.framework/libcrypto_c_exports
 
@@ -97,6 +107,14 @@ for binary in $(printf "%s\n" "${sdk_names[@]}" | sort -u); do
 
   cp ./Headers/*.h ./Frameworks/$binary/libcrypto_c_exports.framework/Headers
   cp ./Info.plist ./Frameworks/$binary/libcrypto_c_exports.framework/Info.plist
+
+  if [[ binary == macosx* ]]; then
+    min_version="$macosx_min_version";
+  else 
+    min_version="$ios_min_version";
+  fi
+
+  $plist_cmd -c "Add :MinimumOSVersion string $min_version" ./Frameworks/$binary/libcrypto_c_exports.framework/Info.plist
 
   build_command+=" -framework Frameworks/$binary/libcrypto_c_exports.framework"
 done
